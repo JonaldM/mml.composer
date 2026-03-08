ffe877fafcbac96bd0aee6b42eabfd1969d5972d
Jonald M <jonaldm@gmail.com>
2026-03-08 21:22:31 +1300

    chore(barcodes): remove old barcodes/ path from index
    Module was migrated to mml.barcodes/ in the previous commit.
    Removes the stale index entries for the old directory.

--- barcodes/mml_barcode_registry/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import mml_brand
-from . import barcode_prefix
-from . import barcode_registry
-from . import barcode_allocation
-from . import product_product
--- barcodes/mml_barcode_registry/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import test_gs1
-from . import test_allocation
-from . import test_generate_sequences
-from . import test_lifecycle
-from . import test_import_wizard
